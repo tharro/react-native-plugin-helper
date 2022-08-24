084cbdc90ce3ccf6984a7cd4f42c7260edbca989
dev au <>
2022-08-24 15:07:46 +0700

    fix(scope): optimise

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import MyPluginHelper from './plugin_helper';
 import MyButtonCustom from './components/button/MyButtonCustom';
-import MyTextInputCustom from './components/textInput/myTextInputCustom';
+import MyTextInputCustom from './components/textInput/MyTextInputCustom';
 
 export { MyPluginHelper, MyButtonCustom, MyTextInputCustom };
