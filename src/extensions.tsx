e8f504fff86d6dc5a36e2b932a1be77d866a69fc
dev au <>
2022-08-25 22:00:40 +0700

    fix(scope): optimise lib

--- src/extensions.tsx
+++ src/extensions.tsx
@@ -1,5 +1,7 @@
+import moment from 'moment';
 import React from 'react';
 import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { PasswordValidType } from './components/textInput/ComponentTextInputCustom';
 
 export const w = (a: number) => {
   return <View style={{ width: a }} />;
@@ -17,3 +19,34 @@ export const HideKeyboard = ({
     {children}
   </TouchableWithoutFeedback>
 );
+
+export const isValidateEmail = (email: string) => {
+  var p: RegExp = new RegExp(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+  return email.match(p);
+};
+
+export const convertUtcToLocalTime = (utc: string) => {
+  return moment.utc(utc).local().format('dd/MM/yyyy HH:mm');
+};
+
+export function isValidToken(expiredToken: number): boolean {
+  const date = new Date(expiredToken * 1000);
+  var minutes: number = moment(moment.now()).diff(date, 'minutes');
+  return minutes > 5;
+}
+
+export function isValidPassword(
+  password: string,
+  passwordValidType: PasswordValidType = PasswordValidType.atLeast8Characters
+) {
+  switch (passwordValidType) {
+    case PasswordValidType.atLeast8Characters:
+      return password.length >= 8;
+    case PasswordValidType.strongPassword:
+      var regexPassword =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.[\]{}()?\-“!@#%&/,><’:;|_~`])\S{8,99}/;
+      return password.match(regexPassword);
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,28 @@
-import MyPluginHelper from './plugin_helper';
 import ComponentButtonCustom from './components/button/ComponentButtonCustom';
 import ComponentTextInputCustom, {
   ValidType,
   PasswordValidType,
 } from './components/textInput/ComponentTextInputCustom';
-import { h, w, HideKeyboard } from './extensions';
-import SvgUri from 'react-native-svg-uri';
-
-export {
-  MyPluginHelper,
-  ComponentButtonCustom,
-  ComponentTextInputCustom,
-  SvgUri,
+import {
   h,
   w,
+  HideKeyboard,
+  convertUtcToLocalTime,
+  isValidPassword,
+  isValidToken,
+  isValidateEmail,
+} from './extensions';
+
+export {
+  ComponentButtonCustom,
+  ComponentTextInputCustom,
   ValidType,
   PasswordValidType,
+  h,
+  w,
+  convertUtcToLocalTime,
+  isValidPassword,
+  isValidToken,
+  isValidateEmail,
   HideKeyboard,
 };
