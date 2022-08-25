a446f7a8c23687482f8a5724c09255f4ec969f1e
dev au <>
2022-08-25 11:00:17 +0700

    fix(scope): fix export

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import MyPluginHelper from './plugin_helper';
 import ComponentButtonCustom from './components/button/ComponentButtonCustom';
 import ComponentTextInputCustom from './components/textInput/ComponentTextInputCustom';
+import { h, w } from './extensions';
+import SvgUri from 'react-native-svg-uri';
 
-export { MyPluginHelper, ComponentButtonCustom, ComponentTextInputCustom };
+export {
+  MyPluginHelper,
+  ComponentButtonCustom,
+  ComponentTextInputCustom,
+  SvgUri,
+  h,
+  w,
+};
