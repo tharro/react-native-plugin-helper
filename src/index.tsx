70feadebbb14442825ed75c877bc992df902436d
dev au <>
2022-08-28 13:59:28 +0700

    fix(scope): export lib

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,14 @@ import {
   isValidToken,
   isValidateEmail,
 } from './extensions';
+import * as SizeScaling from 'react-native-size-scaling';
+import * as VectorIcon from 'react-native-vector-icons';
+import DeviceInfo from 'react-native-device-info';
+import Modal from 'react-native-modal';
+import PushNotification from 'react-native-push-notification';
+import Svg from 'react-native-svg';
+import SvgUri from 'react-native-svg-uri';
+import * as ToastMessage from 'react-native-toast-message';
 
 export {
   ComponentButtonCustom,
@@ -28,4 +36,12 @@ export {
   isValidToken,
   isValidateEmail,
   HideKeyboard,
+  SizeScaling,
+  VectorIcon,
+  DeviceInfo,
+  Modal,
+  PushNotification,
+  Svg,
+  SvgUri,
+  ToastMessage,
 };
