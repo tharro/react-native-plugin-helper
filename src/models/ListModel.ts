5305ac09868a99ebfba70a5f88668cc209f6a44b
dev au <>
2024-07-21 03:47:49 +0700

    fix(scope): update list model

--- src/models/ListModel.ts
+++ src/models/ListModel.ts
@@ -1,8 +1,27 @@
 interface ListModel<T> {
+  /// Total row of the list on the database.
   count?: number;
+
+  /// A URL to get more data from the server.
   next?: string;
-  previous?: string;
+
+  /// List data with custom model.
   results?: T[];
+
+  /// Add parameters when searching or filtering.
+  params: { [key: string]: any };
+
+  /// Waiting for server.
+  isLoading: boolean;
+
+  /// Loading more data from the server.
+  isLoadingMore: boolean;
+
+  /// Refreshing from the server.
+  isRefreshing: boolean;
+
+  /// Error message from the server.
+  errorMessage?: string | null;
 }
 
 export default ListModel;
